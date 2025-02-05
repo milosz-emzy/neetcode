8fae978081b5826a9c44e287d9b925166f29cd18
milosz-emzy <miloszemzy@gmail.com>
2025-02-05 02:53:15 +0100

    Add Binary Search examples

---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,42 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTest {
+
+    @Test
+    void testSearchValid() {
+        int[] input = new int[]{-1, 0, 2, 4, 6, 8};
+
+        Assertions.assertThat(search(input, -1)).isEqualTo(0);
+        Assertions.assertThat(search(input, 0)).isEqualTo(1);
+        Assertions.assertThat(search(input, 2)).isEqualTo(2);
+        Assertions.assertThat(search(input, 4)).isEqualTo(3);
+        Assertions.assertThat(search(input, 6)).isEqualTo(4);
+        Assertions.assertThat(search(input, 8)).isEqualTo(5);
+        Assertions.assertThat(search(input, 3)).isEqualTo(-1);
+        Assertions.assertThat(search(input, -999)).isEqualTo(-1);
+        Assertions.assertThat(search(input, 1000000)).isEqualTo(-1);
+    }
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            //do not use (left+right)/2, because it may overflow INT range
+            // mid = left + ((right-left)/2)
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] == target) {
+                return middle;
+            }
+            if (nums[middle] < target) {
+                left = middle + 1;
+            }
+            if (nums[middle] > target) {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/java/Search2DMatrixTest.java
@@ -0,0 +1,68 @@
+/*
+* You are given an m x n 2-D integer array matrix and an integer target.
+
+    Each row in matrix is sorted in non-decreasing order.
+    The first integer of every row is greater than the last integer of the previous row.
+
+Return true if target exists within matrix or false otherwise.
+* */
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Search2DMatrixTest {
+
+    @Test
+    void testSearchMatrix() {
+        int[][] matrix = new int[][]{{1, 2, 4, 8}, {10, 11, 12, 13}, {14, 20, 30, 40}};
+        Assertions.assertThat(searchMatrix(matrix, 1)).isTrue();
+        Assertions.assertThat(searchMatrix(matrix, 8)).isTrue();
+        Assertions.assertThat(searchMatrix(matrix, 10)).isTrue();
+        Assertions.assertThat(searchMatrix(matrix, 14)).isTrue();
+        Assertions.assertThat(searchMatrix(matrix, 40)).isTrue();
+
+        Assertions.assertThat(searchMatrix(matrix, 15)).isFalse();
+        Assertions.assertThat(searchMatrix(matrix, 0)).isFalse();
+        Assertions.assertThat(searchMatrix(matrix, 100000)).isFalse();
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int l = 0;
+        int r = n - 1;
+
+        while (l <= r) {
+            int m = l + ((r - l) / 2);
+            int leftValue = matrix[m][0];
+            int rightValue = matrix[m][matrix[m].length - 1];
+
+            if (leftValue <= target && rightValue >= target) {
+                //this is matrix with target value
+                int lm = 0, rm = matrix[m].length - 1;
+                while (lm <= rm) {
+                    int mm = lm + ((rm - lm) / 2);
+                    int middleMatrixValue = matrix[m][mm];
+                    if (middleMatrixValue == target) {
+                        return true;
+                    }
+                    if (middleMatrixValue < target) {
+                        lm = mm + 1;
+                    }
+                    if (middleMatrixValue > target) {
+                        rm = mm - 1;
+                    }
+                }
+            }
+
+            if (leftValue < target) {
+                l = m + 1;
+            }
+
+            if (rightValue > target) {
+                r = m - 1;
+            }
+        }
+
+        return false;
+    }
+}
