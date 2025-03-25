ea5a0fa427280f259ed929fae14a2b2db12b1608
milosz-emzy <miloszemzy@gmail.com>
2025-03-25 13:18:22 +0100

    Add ValidParenthesesTest using Map

--- src/test/java/stack/ValidParenthesesTest.java
+++ src/test/java/stack/ValidParenthesesTest.java
@@ -1,10 +1,14 @@
 package stack;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /*
 * You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
 
@@ -21,13 +25,15 @@ class ValidParenthesesTest {
     @ParameterizedTest
     @ValueSource(strings = {"{}", "([{}])"})
     void testValid(String s) {
-        Assertions.assertThat(isValid(s)).isTrue();
+        assertThat(isValid(s)).isTrue();
+        assertThat(isValidWithMap(s)).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"[(])", "]"})
     void testInvalid(String s) {
-        Assertions.assertThat(isValid(s)).isFalse();
+        assertThat(isValid(s)).isFalse();
+        assertThat(isValidWithMap(s)).isFalse();
     }
 
     public boolean isValid(String s) {
@@ -37,9 +43,7 @@ class ValidParenthesesTest {
             switch (c) {
                 case ']' -> {
                     stack.pop();
-                    if (stack.empty() || stack.pop() != '[') {
-                        return false;
-                    }
+                    if (stack.empty() || stack.pop() != '[') return false;
                 }
                 case '}' -> {
                     stack.pop();
@@ -52,5 +56,38 @@ class ValidParenthesesTest {
             }
         }
         return stack.empty();
+    }
+
+    public boolean isValidWithMap(String s) {
+        Stack<Character> stack = new Stack<>();
+        //key closed, value open
+        Map<Character, Character> closed = new HashMap<>();
+        closed.put(']', '[');
+        closed.put('}', '{');
+        closed.put(')', '(');
+
+        for (char c : s.toCharArray()) {
+            //if open push to stack
+            if (closed.containsValue(c)) {
+                stack.push(c);
+            }
+
+            // if closed
+            if (closed.containsKey(c)) {
+
+                // if closed and stack empty "}"
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                // if not matching [} [) {) ...
+                if (!stack.pop().equals(closed.get(c))) {
+                    return false;
+                }
+            }
+        }
+
+        //if all matches stack should be empty
+        return stack.isEmpty();
     }
 }
